85e83c081f7af60deda02165f65c016e35b7d740
CuteMustard <55762397+CuteMustard@users.noreply.github.com>
2020-03-03 12:22:35 -0600

    Update main.dart
    Added top right settings button to map page

--- hs-app/lib/main.dart
+++ hs-app/lib/main.dart
@@ -24,8 +24,41 @@ class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
   final List<Widget> _children = [
     BuildingList(),
-    Container(
+    Stack(
+    children: [Container(
       color: Colors.blue,
+      child: Center(
+        child: Text(
+          'MAP',
+          style: TextStyle(
+            fontSize: 48,
+          ),
+        ),
+      ),
+    ),
+      Align(
+            alignment: Alignment.topRight,
+            child: Transform.scale(
+              scale: 1.75,
+              origin: Offset(-10, -40),
+              child: PopupMenuButton(
+                //splashColor: Color.fromARGB(0, 0, 0, 0),
+                //color: Colors.white,
+                icon: Icon(Icons.more_vert,
+                color: Colors.white), 
+                itemBuilder: (context) =>
+                  <String>['About', 'Feedback', 'Logout']
+                  .map<PopupMenuEntry<String>>((String value) {
+                    return PopupMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+                },).toList(),
+                
+              ),
+            ),
+          ),
+        ],
     ),
   ];
 
